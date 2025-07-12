4921801b878dcc2f06c04a0133935f79fd0399e0
MateenAkram1 <mateen.akram124@gmail.com>
2025-07-12 11:26:49 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,37 +1,30 @@
-const { createServer } = require('@vercel/node');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-const itemRoutes = require('../routes/ItemRoutes');
-const cartRoutes = require('../routes/CartRoutes');
-const orderRoutes = require('../routes/OrderRoutes');
-const userRoutes = require('../routes/UserRoutes');
+import express from 'express';
+import itemRoutes from '../routes/ItemRoutes.js';
+import cartRoutes from '../routes/CartRoutes.js';
+import orderRoutes from '../routes/OrderRoutes.js';
+import userRoutes from '../routes/UserRoutes.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+// DB connect only once
+if (!mongoose.connection.readyState) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
 
-app.get('/', (req, res) => {
-  res.send('🌿 FarmConnect API is running!');
-});
+app.get('/', (req, res) => res.send('FarmConnect API running'));
 
 app.use('/api/items', itemRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/user', userRoutes);
 
-module.exports = app;
+// Export the app as handler
+export default app;
