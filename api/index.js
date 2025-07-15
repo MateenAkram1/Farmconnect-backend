13b4ba00b45042dfa4b1e8ffbe11ed197784bfe7
MateenAkram1 <mateen.akram124@gmail.com>
2025-07-15 13:04:40 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // DB connect only once
